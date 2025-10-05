7bd289a463ecfdbbb59d7ebfe03b8bea49f5101d
Mario <mariomedra2003@gmail.com>
2025-10-05 22:44:26 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
       <Header
         links={[
           { href: "/browse", label: "Browse" },
-          { href: "#features", label: "Features" },
-          { href: "#how-it-works", label: "How it works" },
-          { href: "#use-cases", label: "Use cases" },
+          // { href: "#features", label: "Features" },
+          // { href: "#how-it-works", label: "How it works" },
+          // { href: "#use-cases", label: "Use cases" },
         ]}
       />
 
