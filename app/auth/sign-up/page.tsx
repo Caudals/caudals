f898009bc75336f6ae6766a03fe3ca0b9d42d917
Mario <mariomedra2003@gmail.com>
2025-11-20 19:16:07 +0100

    fixes

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -232,8 +232,8 @@ export default function SignUpPage() {
               <div className="absolute inset-0 flex items-center">
                 <Separator />
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
+              <div className="relative flex justify-center text-xs">
+                <span className="bg-card px-2 text-muted-foreground">
                   {t("Or continue with")}
                 </span>
               </div>
