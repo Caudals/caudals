feff02e2c12cc1ca9173692fa4b93c22f444bbd9
Mario <mariomedra2003@gmail.com>
2025-11-20 19:16:07 +0100

    fixes

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -137,8 +137,8 @@ export default function SignInPage() {
               <div className="absolute inset-0 flex items-center">
                 <Separator />
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
+              <div className="relative flex justify-center text-xs">
+                <span className="bg-card px-2 text-muted-foreground">
                   {t("Or continue with")}
                 </span>
               </div>
